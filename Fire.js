2556609e3f15fbcfc48be20271d2c0a54278cfd5
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2023-08-24 08:14:15 +0530

    finally!

--- Fire.js
+++ Fire.js
@@ -1,157 +1,159 @@
-const firebaseConfig={
-apiKey: "AIzaSyD0GGbyemoB3geMGxGR_cLSi9pudyPO1MM",
-authDomain: "begin-dd0d0.firebaseapp.com",
-databaseURL: "https://begin-dd0d0-default-rtdb.firebaseio.com",
-projectId: "begin-dd0d0",
-storageBucket: "begin-dd0d0.appspot.com",
-messagingSenderId: "1027323844006",
-appId: "1:1027323844006:web:990c8fa86631ac63082b67"
+const firebaseConfig = {
+    apiKey: "AIzaSyD0GGbyemoB3geMGxGR_cLSi9pudyPO1MM",
+    authDomain: "begin-dd0d0.firebaseapp.com",
+    databaseURL: "https://begin-dd0d0-default-rtdb.firebaseio.com",
+    projectId: "begin-dd0d0",
+    storageBucket: "begin-dd0d0.appspot.com",
+    messagingSenderId: "1027323844006",
+    appId: "1:1027323844006:web:990c8fa86631ac63082b67"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const auth=firebase.auth(),
-firestore=firebase.firestore(),
-storage=firebase.storage().ref(),
-hr=new Date().getHours();
+const auth = firebase.auth(),
+    firestore = firebase.firestore(),
+    storage = firebase.storage().ref(),
+    hr = new Date().getHours();
 
 firestore.settings({
-timeStampsInSnapshots: true
+    timeStampsInSnapshots: true
 });
 
-auth.onAuthStateChanged(crd=>{
-if(crd){
-const script=document.createElement("script");
-script.setAttribute("src", "Log.js");
-document.body.appendChild(script);
-document.getElementById("sign").style.display="none";
-mail.innerHTML=crd.email.split("@")[0];
-dis.innerHTML=crd.displayName.split(" ")[0];
-nam.innerHTML=crd.displayName;
-bmail.innerHTML=crd.email;
-document.getElementById("dp").style.backgroundImage="url('"+crd.photoURL+"')";
-id=crd.uid;
-} else{
-document.getElementById("sign").style.display="flex";
-}
+auth.onAuthStateChanged(crd => {
+    if (crd) {
+        const script = document.createElement("script");
+        script.setAttribute("src", "Log.js");
+        document.body.appendChild(script);
+        document.getElementById("sign").style.display = "none";
+        mail.innerHTML = crd.email.split("@")[0];
+        dis.innerHTML = crd.displayName.split(" ")[0];
+        nam.innerHTML = crd.displayName;
+        bmail.innerHTML = crd.email;
+        document.getElementById("dp").style.backgroundImage = "url('" + crd.photoURL + "')";
+        id = crd.uid;
+    } else {
+        document.getElementById("sign").style.display = "flex";
+    }
 });
 
-function signin(dis){
-auth.signInWithEmailAndPassword(dis.email.value, dis.password.value).then(()=>{
-alert("Welcome");
-}).catch(err=>{
-android.toast(err.message);
-});
+function signin(dis) {
+    auth.signInWithEmailAndPassword(dis.email.value, dis.password.value).then(() => {
+        alert("Welcome");
+    }).catch(err => {
+        alert(err.message);
+    });
 }
 
-function register(dis){
-if(dis.password.value==dis.cpass.value){
-auth.createUserWithEmailAndPassword(dis.email.value,dis.password.value).then(cred=>{
-return cred.user.updateProfile({
-displayName: dis.name.value,
-photoURL: "nodp.svg"
-}).then(()=>{
-console.log(cred.user.uid)
-firestore.collection(cred.user.uid).doc("point").set({
-score: 0
-}).then(()=>{
-alert("Thank you!");
-}).catch(err=>{
-android.toast(err.message);
-});
-});
-}).catch(err=>{
-android.toast(err.message);
-});
-} else{
-alert("Confirm your password again");
-dis.password.value="";
-dis.cpass.value="";
-dis.password.focus();
-}
-}
-
-function sin(one, two){
-const o=document.querySelector("#"+one), t=document.querySelector("#"+two), os=document.querySelector("#"+one+">#s"), of=document.querySelector("#"+one+">#f"), oi=document.querySelector("#"+one+">form"), ti=document.querySelector("#"+two+">form"), ts=document.querySelector("#"+two+">#s"), tf=document.querySelector("#"+two+">#f");
-if(o.style.height!="56%"){
-faltu.focus();
-o.style.height="56%";
-o.style.width="78%";
-t.style.width="23%";
-t.style.height="5vh";
-o.style.boxShadow="0 0 99px 12px #0ff";
-t.style.boxShadow="0 0 0 0 #0ff"
-os.style.display="block";
-ti.style.opacity="0";
-setTimeout(()=>{os.style.fontSize="300%"; os.style.border="3px solid snow";
-tf.style.opacity="1";},0)
-of.style.opacity="0";
-ts.style.border="0 solid transparent";
-ts.style.fontSize="0";
-setTimeout(()=>{
-of.style.display="none";
-ti.style.display="none";
-ts.style.display="none";
-setTimeout(()=>{
-oi.style.display="flex";
-oi.style.opacity="1";
-oi["fir"].focus();
-}, 200);
-}, 300);
-tf.style.display="block";
-}
+function register(dis) {
+    if (dis.password.value == dis.cpass.value) {
+        auth.createUserWithEmailAndPassword(dis.email.value, dis.password.value).then(cred => {
+            return cred.user.updateProfile({
+                displayName: dis.name.value,
+                photoURL: "nodp.svg"
+            }).then(() => {
+                console.log(cred.user.uid)
+                firestore.collection(cred.user.uid).doc("point").set({
+                    score: 0
+                }).then(() => {
+                    alert("Thank you!");
+                }).catch(err => {
+                    alert(err.message);
+                });
+            });
+        }).catch(err => {
+            alert(err.message);
+        });
+    } else {
+        alert("Confirm your password again");
+        dis.password.value = "";
+        dis.cpass.value = "";
+        dis.password.focus();
+    }
 }
 
-function ud(dis){
-document.querySelectorAll("form>input").forEach(h=>{
-h.style.borderBottom="0";
-});
-dis.style.borderBottom="7px solid #ffb200";
+function sin(one, two) {
+    const o = document.querySelector("#" + one), t = document.querySelector("#" + two), os = document.querySelector("#" + one + ">#s"), of = document.querySelector("#" + one + ">#f"), oi = document.querySelector("#" + one + ">form"), ti = document.querySelector("#" + two + ">form"), ts = document.querySelector("#" + two + ">#s"), tf = document.querySelector("#" + two + ">#f");
+    if (o.style.height != "56%") {
+        faltu.focus();
+        o.style.height = "56%";
+        o.style.width = "78%";
+        t.style.width = "23%";
+        t.style.height = "5vh";
+        o.style.boxShadow = "0 0 99px 12px #0ff";
+        t.style.boxShadow = "0 0 0 0 #0ff"
+        os.style.display = "block";
+        ti.style.opacity = "0";
+        setTimeout(() => {
+            os.style.fontSize = "300%"; os.style.border = "3px solid snow";
+            tf.style.opacity = "1";
+        }, 0)
+        of.style.opacity = "0";
+        ts.style.border = "0 solid transparent";
+        ts.style.fontSize = "0";
+        setTimeout(() => {
+            of.style.display = "none";
+            ti.style.display = "none";
+            ts.style.display = "none";
+            setTimeout(() => {
+                oi.style.display = "flex";
+                oi.style.opacity = "1";
+                oi["fir"].focus();
+            }, 200);
+        }, 300);
+        tf.style.display = "block";
+    }
 }
 
-function main(aid){
-const sec=document.querySelector("section"), elm=document.getElementById(aid),
-      ap=document.querySelector("#"+aid+">div");
-if(document.querySelector("#"+aid+">div").innerHTML==""){
-setTimeout(()=>{
-elm.style.height="100%";
-elm.style.width="100%";
-}, 123);
-elm.style.left="50%";
-elm.style.transform="translate(-50%, -50%)";
-elm.style.top="50%";
-elm.style.border="0 solid white";
-elm.style.zIndex="1";
-elm.style.background="rgba(0, 19, 28, .7)";
-elm.style.backdropFilter="blur(123px)";
-elm.style.justifyContent="flex-start";
-elm.style.alignItems="stretch";
-setTimeout(()=>{
-elm.style.borderRadius="0";
-document.querySelector("#"+aid+">center").style.display="block";
-}, 250);
-var s, t;
-if(aid=="fruit"){
-s=fruit; t="Fruits";
-} else if(aid=="veg"){
-s=veg; t="Veggies";
-} else if(aid=="grain"){
-s=grain; t="Grains";
-} else if(aid=="milk"){
-s=dairy; t="Dairy";
+function ud(dis) {
+    document.querySelectorAll("form>input").forEach(h => {
+        h.style.borderBottom = "0";
+    });
+    dis.style.borderBottom = "7px solid #ffb200";
 }
-s.once("value").then((f)=>{
-const l=f.val(), fal=Object.keys(l);
-for (var i in l){
-const hel=document.createElement("span"), v=Math.round(l[i]["i"].reduce((a, b) => a + b)/(l[i]["i"].length-1));
-hel.innerHTML="<span>"+i+"</span><span>≈</span><span>₹"+v+" <i id='"+i+"' onclick='plus(this.id, `"+t+"`, `"+v+"`)'>✎</i></span>";
-ap.appendChild(hel);
-ap.style.display="block";
-setTimeout(()=>{
-ap.style.opacity="1";
-}, 999);
-}
-});
-document.querySelector("section>#"+aid+">span").style.display="flex";
-}
+
+function main(aid) {
+    const sec = document.querySelector("section"), elm = document.getElementById(aid),
+        ap = document.querySelector("#" + aid + ">div");
+    if (document.querySelector("#" + aid + ">div").innerHTML == "") {
+        setTimeout(() => {
+            elm.style.height = "100%";
+            elm.style.width = "100%";
+        }, 123);
+        elm.style.left = "50%";
+        elm.style.transform = "translate(-50%, -50%)";
+        elm.style.top = "50%";
+        elm.style.border = "0 solid white";
+        elm.style.zIndex = "1";
+        elm.style.background = "rgba(0, 19, 28, .7)";
+        elm.style.backdropFilter = "blur(123px)";
+        elm.style.justifyContent = "flex-start";
+        elm.style.alignItems = "stretch";
+        setTimeout(() => {
+            elm.style.borderRadius = "0";
+            document.querySelector("#" + aid + ">center").style.display = "block";
+        }, 250);
+        var s, t;
+        if (aid == "fruit") {
+            s = fruit; t = "Fruits";
+        } else if (aid == "veg") {
+            s = veg; t = "Veggies";
+        } else if (aid == "grain") {
+            s = grain; t = "Grains";
+        } else if (aid == "milk") {
+            s = dairy; t = "Dairy";
+        }
+        s.once("value").then((f) => {
+            const l = f.val(), fal = Object.keys(l);
+            for (var i in l) {
+                const hel = document.createElement("span"), v = Math.round(l[i]["i"].reduce((a, b) => a + b) / (l[i]["i"].length - 1));
+                hel.innerHTML = "<span>" + i + "</span><span>≈</span><span>₹" + v + " <i id='" + i + "' onclick='plus(this.id, `" + t + "`, `" + v + "`)'>✎</i></span>";
+                ap.appendChild(hel);
+                ap.style.display = "block";
+                setTimeout(() => {
+                    ap.style.opacity = "1";
+                }, 999);
+            }
+        });
+        document.querySelector("section>#" + aid + ">span").style.display = "flex";
+    }
 }
--- Log.js
+++ Log.js
@@ -1,328 +1,329 @@
-$(document).ready(function(){
-$("#locs").css("color", "white");
-$("#locs").change(function(){
-const curr = $("#locs").val();
-if (curr!="null"){
-$("#locs").css("color", "white");
-} else{
-$("#locs").css("color", "white");
-}
-}); 
+$(document).ready(function () {
+    $("#locs").css("color", "white");
+    $("#locs").change(function () {
+        const curr = $("#locs").val();
+        if (curr != "null") {
+            $("#locs").css("color", "white");
+        } else {
+            $("#locs").css("color", "white");
+        }
+    });
 });
 
 
 var loc, fruit, veg, dairy, grain, id;
-$.get("https://ipinfo.io/json?token=40c851c1d853c3", function(data){
-loc=data.city;
-now.innerHTML=loc;
-now.value=loc;
-}).then(()=>{
-fruit=firebase.database().ref(loc+"/Fruits");
-veg=firebase.database().ref(loc+"/Veggies");
-dairy=firebase.database().ref(loc+"/Dairy");
-grain=firebase.database().ref(loc+"/Grains");
-if(data.country!="IN"){
-document.body.style.display="none";
-alert("This app is not available in your country!");
-}
+$.get("https://ipinfo.io/json?token=40c851c1d853c3", function (data) {
+    loc = data.city;
+    now.innerHTML = loc;
+    now.value = loc;
+}).then(() => {
+    fruit = firebase.database().ref(loc + "/Fruits");
+    veg = firebase.database().ref(loc + "/Veggies");
+    dairy = firebase.database().ref(loc + "/Dairy");
+    grain = firebase.database().ref(loc + "/Grains");
+    if (data.country != "IN") {
+        document.body.style.display = "none";
+        alert("This app is not available in your country!");
+    }
 });
 
-firestore.collection(id).doc("point").get().then(snap=>{
-document.getElementById("scr").innerHTML=snap.data().score;
+firestore.collection(id).doc("point").get().then(snap => {
+    document.getElementById("scr").innerHTML = snap.data().score;
 });
 
-function sinout(){
-if(confirm("Are you sure?")===true){
-auth.signOut().then(()=>{
-alert("You logged out.");
-android.rest();
-});
-}
-}
-
-function add(){
-firebase.database().ref(loc).set({
-Fruits,
-Grains,
-Veggies,
-Dairy
-}).then(()=>{
-android.toast("Sorry, "+loc+" is currently unavailable. It'll be added soon");
-});
+function sinout() {
+    if (confirm("Are you sure?") === true) {
+        auth.signOut().then(() => {
+            alert("You logged out.");
+        });
+    }
 }
 
-firebase.database().ref().once("value").then((city)=>{
-const cities=Object.keys(city.val());
-if(cities.includes(loc)===false){
-add();
-}
-cities.forEach(function(pls){
-if(pls!=loc){ 
-const sel=document.createElement("option");
-sel.innerHTML=pls;
-sel.setAttribute("value", pls);
-other.appendChild(sel);
-}
-});
-});
-
-function update(){
-auth.currentUser.updateProfile({
-photoURL: imur
-}).then(()=>{
-alert("Done!");
-}).catch(err=>{
-android.toast(err.message);
-});
+function add() {
+    firebase.database().ref(loc).set({
+        Fruits,
+        Grains,
+        Veggies,
+        Dairy
+    }).then(() => {
+        alert("Sorry, " + loc + " is currently unavailable. It'll be added soon");
+    });
 }
 
-function upem(){
-var em=prompt("Enter new E-mail", "E-mail");
-auth.currentUser.updateEmail(em).then(()=>{
-alert("E-mail changed successfully!");
-}).catch((error)=>{
-android.toast(err.message);
-});
-}
-
-function upnm(){
-var nm=prompt("Enter new name", "Your name here");
-auth.currentUser.updateProfile({
-displayName: nm
-}).then(()=>{
-alert("Name updated successfully!");
-}, err=>{
-android.toast(err.message);
-});
-}
-
-function chngpass(){
-var np=prompt("Enter new password", "Password"), cnp;
-if(np!="" && np!=null){
-cnp=prompt("Confirm password");
-}
-if(np==cnp){
-auth.currentUser.updatePassword(np).then(()=>{
-alert("Password reset successfully!");
-}, err=>{
-android.toast(err.message);
-});
-} else{
-alert("Enter new password again!");
-chngpass();
-}
-}
-
-function dp(){
-var fil=document.getElementById("file").files[0];
-storage.child(id).put(fil).then(res=>{
-storage.child(id).getDownloadURL().then(url=>{
-document.getElementById("dp").style.backgroundImage="url('"+url+"')";
-auth.currentUser.updateProfile({
-photoURL: url
-});
-});
-});
-}
-
-function plus(j, q, r){
-firebase.database().ref(loc+"/"+q+"/"+j+"/i").once("value").then((f)=>{
-const i=f.val();
-var inp=parseInt(prompt("Enter price", "Price"));
-if(inp!=null && inp!="" && inp<999 && inp>9){
-if(inp>=r && inp-r<50 && r!=null){
-i.push(inp);
-firebase.database().ref(loc+"/"+q+"/"+j).set({
-i
-}).then(()=>{
-firestore.collection(id).doc("point").set({
-score: scr.innerHTML-(-10)
-}).then(()=>{
-alert("You earned some points!");
-scr.innerHTML=scr.innerHTML-(-10);
-});
-});
-} else if(inp<r && r-inp<50 && r!=null){
-i.push(inp);
-firebase.database().ref(loc+"/"+q+"/"+j).set({
-i
-}).then(()=>{
-firestore.collection(id).doc("point").set({
-score: scr.innerHTML-(-10)
-}).then(()=>{
-alert("You earned some points!");
-scr.innerHTML=scr.innerHTML-(-10);
-});
-});
-} else if(isNaN(r)){
-i.push(inp);
-firebase.database().ref(loc+"/"+q+"/"+j).set({
-i
-}).then(()=>{
-firestore.collection(id).doc("point").set({
-score: scr.innerHTML-(-10)
-}).then(()=>{
-alert("You earned some points!");
-scr.innerHTML=scr.innerHTML-(-10);
-});
-});
-} else{
-alert("😑");
-}
-}
-});
-}
-
-document.querySelectorAll("section>div>div").forEach(k=>{
-k.style.height="42%";
+firebase.database().ref().once("value").then((city) => {
+    const cities = Object.keys(city.val());
+    if (cities.includes(loc) === false) {
+        add();
+    }
+    cities.forEach(function (pls) {
+        if (pls != loc) {
+            const sel = document.createElement("option");
+            sel.innerHTML = pls;
+            sel.setAttribute("value", pls);
+            other.appendChild(sel);
+        }
+    });
 });
 
-function band(t, l, b, bid){
-const elm=document.getElementById(bid), ap=document.querySelector("section>#"+bid+">div");
-setTimeout(()=>{elm.style.height="42%";
-elm.style.width="44%";},0)
-setTimeout(()=>{
-elm.style.left=l;
-elm.style.top=t;
-elm.style.justifyContent="center";
-elm.style.alignItems="center";
-elm.style.transform="translate(0, 0)";
-ap.style.opacity="0";
-document.querySelectorAll("section>div>div").forEach(f=>{f.style.height="0"})
-}, 123);
-document.querySelector("#"+bid+">center").style.display="flex";
-elm.style.border="7px solid white";
-ap.style.display="none";
-setTimeout(()=>{
-ap.style.display="none";
-ap.innerHTML="";
-}, 250);
-setTimeout(()=>{
-elm.style.borderRadius="99px";
-elm.style.background=b;
-elm.style.backdropFilter="blur(0)";
-elm.style.zIndex="0";
-}, 345);
-document.querySelector("section>#"+bid+">span").style.display="none";
+function update() {
+    auth.currentUser.updateProfile({
+        photoURL: imur
+    }).then(() => {
+        alert("Done!");
+    }).catch(err => {
+        alert(err.message);
+    });
 }
 
-const Veggies={
-"Potato": {"i": [0]},
-"Onion": {"i": [0]},
-"Ladyfinger": {"i": [0]},
-"Brinjal": {"i": [0]},
-"Cauliflower": {"i": [0]},
-"Corn": {"i": [0]},
-"Garlic": {"i": [0]},
-"Ginger": {"i": [0]},
-"Tomato": {"i": [0]},
-"Pumpkin": {"i": [0]},
-"Gourd": {"i": [0]},
-"Bitter Gourd": {"i": [0]},
-"Drumstick": {"i": [0]},
-"Pointed Gourd": {"i": [0]},
-"Apple Gourd": {"i": [0]},
-"Arum": {"i": [0]},
-"French beans": {"i": [0]},
-"Beans": {"i": [0]},
-"Carrot": {"i": [0]},
-"Radish": {"i": [0]},
-"Beetroot": {"i": [0]},
-"Papaya": {"i": [0]},
-"Turnip": {"i": [0]},
-"Mushroom": {"i": [0]},
-"Peas": {"i": [0]},
-"Sweet Potato": {"i": [0]},
-"Cabbage": {"i": [0]},
-"Coriander": {"i": [0]},
-"Spinach": {"i": [0]},
-"Snake Gourd": {"i": [0]},
-"Fenugreek": {"i": [0]}
+function upem() {
+    var em = prompt("Enter new E-mail", "E-mail");
+    auth.currentUser.updateEmail(em).then(() => {
+        alert("E-mail changed successfully!");
+    }).catch((error) => {
+        alert(err.message);
+    });
+}
+
+function upnm() {
+    var nm = prompt("Enter new name", "Your name here");
+    auth.currentUser.updateProfile({
+        displayName: nm
+    }).then(() => {
+        alert("Name updated successfully!");
+    }, err => {
+        alert(err.message);
+    });
+}
+
+function chngpass() {
+    var np = prompt("Enter new password", "Password"), cnp;
+    if (np != "" && np != null) {
+        cnp = prompt("Confirm password");
+    }
+    if (np == cnp) {
+        auth.currentUser.updatePassword(np).then(() => {
+            alert("Password reset successfully!");
+        }, err => {
+            alert(err.message);
+        });
+    } else {
+        alert("Enter new password again!");
+        chngpass();
+    }
+}
+
+function dp() {
+    var fil = document.getElementById("file").files[0];
+    storage.child(id).put(fil).then(res => {
+        storage.child(id).getDownloadURL().then(url => {
+            document.getElementById("dp").style.backgroundImage = "url('" + url + "')";
+            auth.currentUser.updateProfile({
+                photoURL: url
+            });
+        });
+    });
+}
+
+function plus(j, q, r) {
+    firebase.database().ref(loc + "/" + q + "/" + j + "/i").once("value").then((f) => {
+        const i = f.val();
+        var inp = parseInt(prompt("Enter price", "Price"));
+        if (inp != null && inp != "" && inp < 999 && inp > 9) {
+            if (inp >= r && inp - r < 50 && r != null) {
+                i.push(inp);
+                firebase.database().ref(loc + "/" + q + "/" + j).set({
+                    i
+                }).then(() => {
+                    firestore.collection(id).doc("point").set({
+                        score: scr.innerHTML - (-10)
+                    }).then(() => {
+                        alert("You earned some points!");
+                        scr.innerHTML = scr.innerHTML - (-10);
+                    });
+                });
+            } else if (inp < r && r - inp < 50 && r != null) {
+                i.push(inp);
+                firebase.database().ref(loc + "/" + q + "/" + j).set({
+                    i
+                }).then(() => {
+                    firestore.collection(id).doc("point").set({
+                        score: scr.innerHTML - (-10)
+                    }).then(() => {
+                        alert("You earned some points!");
+                        scr.innerHTML = scr.innerHTML - (-10);
+                    });
+                });
+            } else if (isNaN(r)) {
+                i.push(inp);
+                firebase.database().ref(loc + "/" + q + "/" + j).set({
+                    i
+                }).then(() => {
+                    firestore.collection(id).doc("point").set({
+                        score: scr.innerHTML - (-10)
+                    }).then(() => {
+                        alert("You earned some points!");
+                        scr.innerHTML = scr.innerHTML - (-10);
+                    });
+                });
+            } else {
+                alert("😑");
+            }
+        }
+    });
+}
+
+document.querySelectorAll("section>div>div").forEach(k => {
+    k.style.height = "42%";
+});
+
+function band(t, l, b, bid) {
+    const elm = document.getElementById(bid), ap = document.querySelector("section>#" + bid + ">div");
+    setTimeout(() => {
+        elm.style.height = "42%";
+        elm.style.width = "44%";
+    }, 0)
+    setTimeout(() => {
+        elm.style.left = l;
+        elm.style.top = t;
+        elm.style.justifyContent = "center";
+        elm.style.alignItems = "center";
+        elm.style.transform = "translate(0, 0)";
+        ap.style.opacity = "0";
+        document.querySelectorAll("section>div>div").forEach(f => { f.style.height = "0" })
+    }, 123);
+    document.querySelector("#" + bid + ">center").style.display = "flex";
+    elm.style.border = "7px solid white";
+    ap.style.display = "none";
+    setTimeout(() => {
+        ap.style.display = "none";
+        ap.innerHTML = "";
+    }, 250);
+    setTimeout(() => {
+        elm.style.borderRadius = "99px";
+        elm.style.background = b;
+        elm.style.backdropFilter = "blur(0)";
+        elm.style.zIndex = "0";
+    }, 345);
+    document.querySelector("section>#" + bid + ">span").style.display = "none";
+}
+
+const Veggies = {
+    "Potato": { "i": [0] },
+    "Onion": { "i": [0] },
+    "Ladyfinger": { "i": [0] },
+    "Brinjal": { "i": [0] },
+    "Cauliflower": { "i": [0] },
+    "Corn": { "i": [0] },
+    "Garlic": { "i": [0] },
+    "Ginger": { "i": [0] },
+    "Tomato": { "i": [0] },
+    "Pumpkin": { "i": [0] },
+    "Gourd": { "i": [0] },
+    "Bitter Gourd": { "i": [0] },
+    "Drumstick": { "i": [0] },
+    "Pointed Gourd": { "i": [0] },
+    "Apple Gourd": { "i": [0] },
+    "Arum": { "i": [0] },
+    "French beans": { "i": [0] },
+    "Beans": { "i": [0] },
+    "Carrot": { "i": [0] },
+    "Radish": { "i": [0] },
+    "Beetroot": { "i": [0] },
+    "Papaya": { "i": [0] },
+    "Turnip": { "i": [0] },
+    "Mushroom": { "i": [0] },
+    "Peas": { "i": [0] },
+    "Sweet Potato": { "i": [0] },
+    "Cabbage": { "i": [0] },
+    "Coriander": { "i": [0] },
+    "Spinach": { "i": [0] },
+    "Snake Gourd": { "i": [0] },
+    "Fenugreek": { "i": [0] }
 },
 
-Fruits={
-"Mango":{"i": [0]},
-"Banana": {"i": [0]},
-"Apple": {"i": [0]},
-"Custard Apple": {"i": [0]},
-"Pomegranate": {"i": [0]},
-"Watermelon": {"i": [0]},
-"Melon": {"i": [0]},
-"Peach": {"i": [0]},
-"Kiwi": {"i": [0]},
-"Strawberry": {"i": [0]}
-},
+    Fruits = {
+        "Mango": { "i": [0] },
+        "Banana": { "i": [0] },
+        "Apple": { "i": [0] },
+        "Custard Apple": { "i": [0] },
+        "Pomegranate": { "i": [0] },
+        "Watermelon": { "i": [0] },
+        "Melon": { "i": [0] },
+        "Peach": { "i": [0] },
+        "Kiwi": { "i": [0] },
+        "Strawberry": { "i": [0] }
+    },
 
-Dairy={
-"Paneer": {"i": [0]},
-"Butter": {"i": [0]},
-"Milk": {"i": [0]},
-"Curd": {"i": [0]}
-},
+    Dairy = {
+        "Paneer": { "i": [0] },
+        "Butter": { "i": [0] },
+        "Milk": { "i": [0] },
+        "Curd": { "i": [0] }
+    },
 
-Grains={
-"Rice": {"i": [0]},
-"Cereals": {"i": [0]},
-"Pulse": {"i": [0]},
-"Saffron": {"i": [0]}
-};
+    Grains = {
+        "Rice": { "i": [0] },
+        "Cereals": { "i": [0] },
+        "Pulse": { "i": [0] },
+        "Saffron": { "i": [0] }
+    };
 
-function op(){
-blr.style.display="block";
-setTimeout(()=>{
-blr.style.opacity="1";
-blr.style.backdropFilter="blur(9px)";
-}, 0);
-sid.style.width="50%";
+function op() {
+    blr.style.display = "block";
+    setTimeout(() => {
+        blr.style.opacity = "1";
+        blr.style.backdropFilter = "blur(9px)";
+    }, 0);
+    sid.style.width = "50%";
 }
 
-function cls(){
-blr.style.opacity="0";
-blr.style.backdropFilter="blur(0)";
-setTimeout(()=>{
-blr.style.display="none";
-}, 300);
-sid.style.width="0";
+function cls() {
+    blr.style.opacity = "0";
+    blr.style.backdropFilter = "blur(0)";
+    setTimeout(() => {
+        blr.style.display = "none";
+    }, 300);
+    sid.style.width = "0";
 }
 
-document.querySelectorAll(".ripple").forEach(ripple=>{
-ripple.addEventListener("click", function(point){
-const ripples=document.createElement("i");
-ripples.classList.add("pani");
-ripples.style.left=point.clientX-point.target.offsetLeft+"px";
-ripples.style.top=point.clientY-point.target.offsetTop-300+"px";
-this.appendChild(ripples);
-setTimeout(()=>{
-ripples.remove();
-}, 1010);
+document.querySelectorAll(".ripple").forEach(ripple => {
+    ripple.addEventListener("click", function (point) {
+        const ripples = document.createElement("i");
+        ripples.classList.add("pani");
+        ripples.style.left = point.clientX - point.target.offsetLeft + "px";
+        ripples.style.top = point.clientY - point.target.offsetTop - 300 + "px";
+        this.appendChild(ripples);
+        setTimeout(() => {
+            ripples.remove();
+        }, 1010);
+    });
 });
-});
 
-function chng(dis){
-loc=dis.value;
-fruit=firebase.database().ref(loc+"/Fruits");
-veg=firebase.database().ref(loc+"/Veggies");
-dairy=firebase.database().ref(loc+"/Dairy");
-grain=firebase.database().ref(loc+"/Grains");
+function chng(dis) {
+    loc = dis.value;
+    fruit = firebase.database().ref(loc + "/Fruits");
+    veg = firebase.database().ref(loc + "/Veggies");
+    dairy = firebase.database().ref(loc + "/Dairy");
+    grain = firebase.database().ref(loc + "/Grains");
 }
 
-if(hr<12){
-wish.innerHTML+="Morning,";
-} else if(hr<18){
-wish.innerHTML+="Afternoon,";
-}else {
-wish.innerHTML+="Evening,";
+if (hr < 12) {
+    wish.innerHTML += "Morning,";
+} else if (hr < 18) {
+    wish.innerHTML += "Afternoon,";
+} else {
+    wish.innerHTML += "Evening,";
 }
 
-function duser(){
-if(confirm("Your account will be deleted permanently.")===true){
-firestore.collection(id).doc("point").delete();
-storage.child(id).delete();
-auth.currentUser.delete().then(()=>{
-alert("User deleted.");
-android.rest();
-}).catch(err=>{
-alert(err.message);
-});
-}
+function duser() {
+    if (confirm("Your account will be deleted permanently.") === true) {
+        firestore.collection(id).doc("point").delete();
+        storage.child(id).delete();
+        auth.currentUser.delete().then(() => {
+            alert("User deleted.");
+           
+        }).catch(err => {
+            alert(err.message);
+        });
+    }
 }
