d6a9dd3f2594574fc44abe12a7534a226be44352
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2024-01-17 09:43:45 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'my-pwa-cache';
+
+const filesToCache = [
+  "/",
+  "Abstract.jpg",
+  "Fire.js",
+  "style.css",
+  "Firebase8.js",
+  "index.html",
+  "jQuery.js",
+  "nodp.svg",
+  "Product.ttf"
+];
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
